fd7e4a46af6f85fd8a95dfc1cc1aa5285b0d1b9e
Talut Salako <salakotalut@gmail.com>
2025-06-16 20:42:10 +0100

    set config module to global

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from "@nestjs/common";
 import { AuthModule } from "./auth/auth.module";
-import { UserModule } from './user/user.module';
-import { WalletModule } from './wallet/wallet.module';
+import { UserModule } from "./user/user.module";
+import { WalletModule } from "./wallet/wallet.module";
+import { PrismaModule } from "./prisma/prisma.module";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-	imports: [AuthModule, UserModule, WalletModule],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+		}),
+		AuthModule,
+		UserModule,
+		WalletModule,
+		PrismaModule,
+	],
 })
 export class AppModule {}
